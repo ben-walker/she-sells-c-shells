9bb6e97f80f4a408872a88ab4e695d040e5bb084
Ben Walker <bwalke05@gmail.com>
2019-01-12 13:47:13 -0500

    Add some error detection code

--- src/shell_data_grabber.c
+++ src/shell_data_grabber.c
@@ -3,17 +3,29 @@
 #include <unistd.h>
 #include <pwd.h>
 #include <string.h>
+#include <stdio.h>
 
 const int MACHINE_BOUND = 50;
 
 char *getUsername() {
     const struct passwd *pws = getpwuid(getuid());
+    if (!pws) {
+        fprintf(stderr, "Could not find username.");
+        exit(EXIT_FAILURE);
+    }
     return pws->pw_name;
 }
 
 char *getMachine() {
     char *machine = malloc(MACHINE_BOUND);
-    gethostname(machine, MACHINE_BOUND);
+    if (!machine) {
+        fprintf(stderr, "Memory allocation failed.");
+        exit(EXIT_FAILURE);
+    }
+    if (gethostname(machine, MACHINE_BOUND) != 0) {
+        fprintf(stderr, "Could not find hostname.");
+        exit(EXIT_FAILURE);
+    }
     return machine;
 }
 
@@ -24,5 +36,9 @@ bool isUserRoot() {
 void populate(User *shellUser) {
     shellUser->username = strdup(getUsername());
     shellUser->machine = strdup(getMachine());
+    if (!shellUser->username || !shellUser->machine) {
+        fprintf(stderr, "Memory allocation failed.");
+        exit(EXIT_FAILURE);
+    }
     shellUser->isRoot = isUserRoot();
 }
