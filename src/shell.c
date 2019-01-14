b71fbbb5928f498fa99908442c2b472d3f229078
Ben Walker <bwalke05@gmail.com>
2019-01-13 19:56:26 -0500

    Get user input at the prompt in a loop

--- src/shell.c
+++ src/shell.c
@@ -1,11 +1,25 @@
 #include "shell.h"
 #include "shell_data_grabber.h"
+#include <stdio.h>
+#include <string.h>
 
 const char *DEF_PROMPT = "$";
 const char *SU_PROMPT = "#";
 
+void commandLoop();
+
 void boot() {
     User *shellUser = newUser();
     populate(shellUser);
-    char *prompt = getPrompt(shellUser, DEF_PROMPT, SU_PROMPT);
+    const char *prompt = getPrompt(shellUser, DEF_PROMPT, SU_PROMPT);
+    commandLoop(prompt);
+}
+
+void commandLoop(const char *prompt) {
+    char *buff = NULL;
+    size_t allocSize = 0;
+
+    while (printf("%s ", prompt) >= 0 && getline(&buff, &allocSize, stdin) != -1) {
+        buff[strcspn(buff, "\r\n")] = 0;
+    }
 }
