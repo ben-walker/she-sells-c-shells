389f018c64f2a4cc1c4f79b26ec783dd0c79edf8
Ben Walker <bwalke05@gmail.com>
2019-01-18 22:53:18 -0500

    Finish defining gcd built-in

--- src/gcd.c
+++ src/gcd.c
@@ -1,10 +1,26 @@
 #include "gcd.h"
-#include <stdbool.h>
+#include <stdlib.h>
+#include <limits.h>
 
-bool isHex(const char *num) {
+int convertToNum(const char *val, int *numeric) {
+    char *end = NULL;
+    long converted = strtol(val, &end, 0);
 
+    if (converted == LONG_MAX ||
+        converted == LONG_MIN ||
+        end == val) return EXIT_FAILURE;
+    *numeric = converted;
+    return EXIT_SUCCESS;
+}
+
+int recursiveGcd(const int numOne, const int numTwo) {
+    if (numTwo != 0) return(recursiveGcd(numTwo, numOne % numTwo));
+    else return numOne;
 }
 
 int gcd(const char *valOne, const char *valTwo) {
-
+    int numOne, numTwo;
+    if (convertToNum(valOne, &numOne) == EXIT_FAILURE ||
+        convertToNum(valTwo, &numTwo) == EXIT_FAILURE) return -1;
+    return recursiveGcd(numOne, numTwo);
 }
