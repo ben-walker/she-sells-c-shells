c8b5161b98fdee2b6592d4a4ccb03e4b71153fab
Ben Walker <bwalke05@gmail.com>
2019-01-31 16:18:03 -0500

    Add function to detect if command should be run in the background

--- src/external.c
+++ src/external.c
@@ -3,6 +3,14 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool background(char **args) {
+    char *lastArg;
+    for (int i = 0; args[i] != NULL; lastArg = args[i], i += 1);
+    return strcmp(lastArg, "&") == 0;
+}
 
 int runExternal(char **args) {
     pid_t pid = fork();
