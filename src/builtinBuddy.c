62773eb9b5315635c22a6de46c1593ed3e32e8c0
Ben Walker <bwalke05@gmail.com>
2019-01-30 21:17:03 -0500

    Implement 'args' builtin

--- src/builtinBuddy.c
+++ src/builtinBuddy.c
@@ -14,6 +14,38 @@ int shellGCD(const char *cmd, const char *num1, const char *num2) {
     return EXIT_SUCCESS;
 }
 
+int shellArgs(const char *cmd, char **args) {
+    int argc;
+    char *argsList = NULL, *format;
+    size_t space;
+
+    for (argc = 1; args[argc] != NULL; argc += 1) {
+        format = args[argc + 1] == NULL ? "%s%s" : "%s%s, ";
+        space = snprintf(NULL, 0, format, argsList, args[argc]) + 1;
+        if (space < 0) {
+            fprintf(stderr, "Could not determine size.");
+            exit(EXIT_FAILURE);
+        }
+        argsList = realloc(argsList, space);
+        if (!argsList) {
+            perror("Memory allocation failed.");
+            exit(EXIT_FAILURE);
+        }
+        if (sprintf(argsList, format, argsList, args[argc]) < 0) {
+            fprintf(stderr, "String write failed.");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (!argsList) {
+        fprintf(stderr, "Usage: %s ...options...\n", cmd);
+        return EXIT_FAILURE;
+    }
+    printf("argc = %d, args = %s\n", argc - 1, argsList);
+    free(argsList);
+    return EXIT_SUCCESS;
+}
+
 bool internalCmd(const char *cmd) {
     const char *builtins[] = { GCD, ARGS };
     int len = sizeof(builtins) / sizeof(builtins[0]);
@@ -30,5 +62,5 @@ void runInternal(char **args) {
     if (strcmp(cmd, GCD) == 0)
         shellGCD(args[0], args[1], args[2]);
     else if (strcmp(cmd, ARGS) == 0)
-        NULL;
+        shellArgs(ARGS, args);
 }
