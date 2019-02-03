c144e8b23c7587ce2d803774d896fdbd2e3c0385
Ben Walker <bwalke05@gmail.com>
2019-02-02 22:11:26 -0500

    Remove unused #includes

--- src/external.c
+++ src/external.c
@@ -1,11 +1,8 @@
 #include "external.h"
 #include "specialArgs.h"
-#include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
 
 void runExternal(char **argv) {
     char **newArgv = assignRedirections(argv);
