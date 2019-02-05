b32cc2cffb482af5ac1b59794ce0516446bc24f2
Ben Walker <bwalke05@gmail.com>
2019-02-05 16:28:52 -0500

    Simple linked list to record active processes

---
+++ include/processList.h
@@ -0,0 +1,19 @@
+#ifndef PROCESS_LIST_
+#define PROCESS_LIST_
+
+#include <stdbool.h>
+#include <sys/types.h>
+
+typedef struct PCB {
+    pid_t pid;
+    bool background;
+    struct PCB *next;
+} PCB;
+
+void trackProcess(const pid_t pid, bool background);
+
+void removeProcess(const pid_t pid);
+
+void procPeek();
+
+#endif // PROCESS_LIST_
---
+++ src/processList.c
@@ -0,0 +1,51 @@
+#include "processList.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+static PCB *procs = NULL;
+
+void createProcList(const pid_t pid, bool background) {
+    procs = malloc(sizeof(PCB));
+    procs->pid = pid;
+    procs->background = background;
+    procs->next = NULL;
+}
+
+void trackProcess(const pid_t pid, bool background) {
+    if (procs == NULL) {
+        createProcList(pid, background);
+        return;
+    }
+    PCB *current = procs;
+    while (current->next != NULL)
+        current = current->next;
+    current->next = malloc(sizeof(PCB));
+    current->next->pid = pid;
+    current->next->background = background;
+    current->next->next = NULL;
+}
+
+void removeProcess(const pid_t pid) {
+    PCB *current, *previous = NULL;
+
+    for (current = procs; current != NULL; previous = current, current = current->next) {
+        if (current->pid == pid) {
+            if (previous == NULL)
+                procs = current->next;
+            else
+                previous->next = current->next;
+            free(current);
+            return;
+        }
+    }
+}
+
+void procPeek() {
+    PCB *current = procs;
+    printf("Processes:\n");
+    while (current != NULL) {
+        const char *status = current->background ? "background" : "foreground";
+        printf("  [%d] -> %s\n", current->pid, status);
+        current = current->next;
+    }
+}
