ccbaf5ce2e76820f86e8ae8b44cdf9136dad8ed9
Ben Walker <bwalke05@gmail.com>
2019-02-06 13:56:20 -0500

    Documentation

--- include/external.h
+++ include/external.h
@@ -1,3 +1,10 @@
+/**
+ * Ben Walker
+ * CIS*3110
+ * 
+ * A simple library to run an external command
+ */
+
 #ifndef EXTERNAL_
 #define EXTERNAL_
 
--- src/external.c
+++ src/external.c
@@ -1,12 +1,23 @@
+/**
+ * Ben Walker
+ * CIS*3110
+ * 
+ * A simple library to run an external command
+ */
+
 #include "external.h"
 #include "argsHandler.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * runExternal()
+ * Execute an external shell command
+ */
 void runExternal(char **argv) {
-    char **newArgv = consumeSpecialArgs(argv);
+    char **newArgv = consumeSpecialArgs(argv); // consume file I/O redirections
     execvp(newArgv[0], newArgv);
-    perror("execvp");
+    perror("execvp"); // execvp only returns if there's an error
     exit(EXIT_FAILURE);
 }
