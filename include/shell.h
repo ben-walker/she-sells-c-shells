2d01af703d3d8d50d46a7e1ff52ee9d94395d43e
Ben Walker <bwalke05@gmail.com>
2019-02-06 13:20:24 -0500

    Documentation

--- include/shell.h
+++ include/shell.h
@@ -1,3 +1,14 @@
+/**
+ * Ben Walker
+ * CIS*3110
+ * 
+ * The main command loop for the shell.
+ * Set up a signal handler for SIGCHLD signals,
+ * get the next user command in a loop,
+ * fork() to run/wait for the command,
+ * cleanup memory when the user exits.
+ */
+
 #ifndef SHELL_
 #define SHELL_
 
