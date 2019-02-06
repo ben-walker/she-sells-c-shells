e5a42563d84207c24996e38472e0f08a0ea51a1c
Ben Walker <bwalke05@gmail.com>
2019-02-06 14:11:10 -0500

    Documentation

--- include/argsBuiltin.h
+++ include/argsBuiltin.h
@@ -1,7 +1,14 @@
+/**
+ * Ben Walker
+ * CIS*3110
+ * 
+ * Implementation of the 'args' builtin command
+ */
+
 #ifndef ARGS_BUILTIN_
 #define ARGS_BUILTIN_
 
-extern const char *ARGS_CMD;
+extern const char *ARGS_CMD; // name of the command for external use
 
 void argsIndex(char **argv, char **args, int *argc);
 
--- src/argsBuiltin.c
+++ src/argsBuiltin.c
@@ -1,28 +1,46 @@
+/**
+ * Ben Walker
+ * CIS*3110
+ * 
+ * Implementation of the 'args' builtin command
+ */
+
 #include "argsBuiltin.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 const char *ARGS_CMD = "args";
 
+/**
+ * calcArgc()
+ * Calculate the number of arguments passed to a command
+ * (i.e. length of argv - 1)
+ */
 int calcArgc(char **argv) {
     int argc;
     for (argc = 1; argv[argc] != NULL; argc += 1);
     return argc - 1;
 }
 
+/**
+ * argsIndex()
+ * Populate args with the arguments passed to a command,
+ * and argc with the number of arguments passed to a command
+ * (the 'command' is argv[0])
+ */
 void argsIndex(char **argv, char **args, int *argc) {
     char *format;
     size_t space;
     *argc = calcArgc(argv);
 
     for (int i = 1; argv[i] != NULL; i += 1) {
-        format = argv[i + 1] == NULL ? "%s%s" : "%s%s, ";
+        format = argv[i + 1] == NULL ? "%s%s" : "%s%s, "; // no comma if last element
         space = snprintf(NULL, 0, format, *args, argv[i]) + 1;
         if (space < 0) {
             fprintf(stderr, "Could not determine size.");
             exit(EXIT_FAILURE);
         }
-        *args = realloc(*args, space);
+        *args = realloc(*args, space); // add the required space
         if (!*args) {
             perror("realloc");
             exit(EXIT_FAILURE);
